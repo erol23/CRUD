71707ea019bc79de5ba0d544870c062228e3cf14
erol <hurkanrol15@gmail.com>
2021-07-15 22:20:57 +0300

    add bootstrap

--- src/clarusway/settings.py
+++ src/clarusway/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
     'fscohort',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +139,4 @@ STATICFILES_DIRS=[BASE_DIR / 'static_dir']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- src/fscohort/forms.py
+++ src/fscohort/forms.py
@@ -5,5 +5,14 @@ class StudentForm(forms.ModelForm):
 
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = (
+            'first_name',
+            'last_name',
+            'number'
+        )
         label = {'first_name' : "Adınız"}
+        # widgets = {
+        #     'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'number': forms.NumberInput(attrs={'class': 'form-control'})
+        # }
