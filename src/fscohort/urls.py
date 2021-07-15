cd2f0a40a1cd0b3df36934d7b322aec1d3f247b3
erol <hurkanrol15@gmail.com>
2021-07-15 23:47:47 +0300

    add classbase

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import home_page, student_list, student_add, student_detail, student_update,student_delete
+from .views import home_page, student_list, student_add, student_detail, student_update,student_delete, HomeView,StudentList, StudentCreate, StudentDetail,StudentUpdate, StudentDelete
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', home_page, name='home'),
-    path('list/', student_list, name='list'),
+    # path('', home_page, name='home'),
+    path('', HomeView.as_view(), name='home'),
+    # path('list/', student_list, name='list'),
+    path('list/', StudentList.as_view(), name='list'),
     path('add/', student_add, name='add'),
-    path('<int:id>/', student_detail, name='detail'),
-    path('<int:id>/update/', student_update, name='update'),
-    path('<int:id>/delete/', student_delete, name='delete'),
+    # path('add/', StudentCreate.as_view(), name='add'),
+    # path('<int:id>/', student_detail, name='detail'),
+    path('<int:pk>/', StudentDetail.as_view(), name='detail'),
+    # path('<int:id>/update/', student_update, name='update'),
+    path('<int:pk>/update/', StudentUpdate.as_view(), name='update'),
+    # path('<int:id>/delete/', student_delete, name='delete'),
+    path('<int:pk>/delete/', StudentDelete.as_view(), name='delete'),
 ]
--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from .models import Student
 from .forms import StudentForm
+from django.contrib import messages
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
 
 # def home(request):
 #     return HttpResponse("Hello World!")
 
 def home_page(request):
     return render(request, 'fscohort/home.html')
+
+class HomeView(TemplateView):
+    template_name = 'fscohort/home.html'
 
 def student_list(request):
     students = Student.objects.all()
@@ -18,6 +25,12 @@ def student_list(request):
 
     return render(request, 'fscohort/student_list.html', context)
 
+class StudentList(ListView):
+    model = Student
+    # template_name  # default app/student_list.html
+    context_object_name = 'students'
+    paginate_by = 1
+
 def student_add(request):
     form = StudentForm()
     if request.method == 'POST':
@@ -25,11 +38,19 @@ def student_add(request):
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Student succesfully created ...')
             return redirect("list")
     context = {
         'form': form,
     }
     return render(request, 'fscohort/student_add.html', context)
+
+class StudentCreate(CreateView):
+    model = Student
+    # fields = ('first_name')
+    form_class = StudentForm
+    template_name = 'fscohort/student_add.html'
+    success_url = reverse_lazy('list')
 
 def student_detail(request, id):
     student = Student.objects.get(id=id)
@@ -37,6 +58,9 @@ def student_detail(request, id):
         'student' : student
     }
     return render(request, 'fscohort/student_detail.html', context)
+
+class StudentDetail(DetailView):
+    model = Student
 
 def student_update(request, id):
     student = get_object_or_404(Student, id=id)
@@ -52,6 +76,12 @@ def student_update(request, id):
     }
     return render(request, 'fscohort/student_update.html', context)
 
+class StudentUpdate(UpdateView):
+    model=Student
+    form_class = StudentForm
+    template_name = 'fscohort/student_update.html'  # app/student_form.html
+    success_url = reverse_lazy('list') 
+
 def student_delete(request, id):
     student = get_object_or_404(Student, id=id)
     if request.method == 'POST':
@@ -63,3 +93,8 @@ def student_delete(request, id):
     }
 
     return render(request, 'fscohort/student_delete.html', context)
+
+class StudentDelete(DeleteView):
+    model = Student
+    template_name = 'fscohort/student_delete.html'  # default app/student_confirm_delete.html
+    success_url = reverse_lazy('list')
